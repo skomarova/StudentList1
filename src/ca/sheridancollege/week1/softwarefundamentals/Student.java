a3d4b9c10ade33b2a1f3d5e3a3f0907bdffddd12
skomarova <komarovs@sheridancollege.ca>
2021-03-07 19:38:49 -0500

    5-numHours

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -9,6 +9,26 @@ public class Student {
 
     private String name;
 
+    private String numHours;
+
+    /**
+     * Get the value of numHours
+     *
+     * @return the value of numHours
+     */
+    public String getNumHours() {
+        return numHours;
+    }
+
+    /**
+     * Set the value of numHours
+     *
+     * @param numHours new value of numHours
+     */
+    public void setNumHours(String numHours) {
+        this.numHours = numHours;
+    }
+
     /**
      * Get the value of name
      *
